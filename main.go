fb9923003f4ae9160918ef0e50cd390dd2bcb09b
buscaroli <m.buscaroli@outlook.com>
2022-12-11 19:12:46 +0000

    add very basic templates for home and about pages

--- main.go
+++ main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const port = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Home!")
+	renderTemplate(w, "home.page.html")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello About!")
+	renderTemplate(w, "about.page.html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
 }
 
 func main() {
