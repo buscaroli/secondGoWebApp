1ba2d1d7926926a103d611bebf97eb5fa5caa968
buscaroli <m.buscaroli@outlook.com>
2022-12-12 12:58:11 +0000

    add a cache to store the generated template once a page has been visited to avoid wasting resources everytime the page is pinged

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,15 +2,54 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
+// Caching templates: tc is a map that stores the string:template of every page that has already been visited.
+// eg []
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check if template already in the map
+	_, inMap := tc[t]
+
+	if !inMap {
+		// create the template and add it to the map
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		// read the template from the map
+		log.Println("Using cached page")
 	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t), "./templates/base.layout.html",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	// add template to the map (our cache)
+	tc[t] = tmpl
+	return nil
 }
