bf8c26b416fbc3caa5fc9e746f86f9c8e920cf44
buscaroli <m.buscaroli@outlook.com>
2022-12-18 16:20:30 +0000

    add a session manager to keep hold of data inbetween session and make them expire in 24h. Try it out with some simple logic

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -6,15 +6,23 @@ import (
 	"github.com/justinas/nosurf"
 )
 
+// NoSurf adds CSRF protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.IsProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 
 	return csrfHandler
+}
+
+// adding state to rememeber the session
+// SessionLoad loads and savs the session on every request
+// nb on every request, not every page load: it can be fired more than once if the pages requests multiple files!
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
